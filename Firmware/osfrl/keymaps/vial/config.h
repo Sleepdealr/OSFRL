58941ecf37bbf13f574edcc6bb4ec72e435f5b5a
Sleepdealer <35879391+Sleepdealr@users.noreply.github.com>
2023-02-12 22:01:16 -0500

    init to winit

---
+++ Firmware/osfrl/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xAB, 0x92, 0x93, 0x5A, 0xF5, 0xF3, 0xD3, 0xE7}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 4}
+#define VIAL_UNLOCK_COMBO_COLS {0, 0}
---
+++ README.md
@@ -0,0 +1,7 @@
+# OSFRL
+
+FRL PCB made with STM32F072 and Molex EZMate
+
+Source PCB files provided in the `PCB/` folder, with firmware source in the `Firmware/` folder
+
+These have been tested and are working
